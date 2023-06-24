d0bd6972611d5a86e4a512223b70aaea06222978
nischay <nischey.me008@gmail.com>
2023-06-24 21:30:45 +0530

    24 june

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <?php 
 class class1{
-    function fun1(){
-        echo "fun1";
+    function __construct($y)
+    {
+        $this->x=$y;
+        echo "start";
     }
-    function fun2(){
-        echo "fun2";
+    function fun1()
+    {
+        return $this->x;
+        // echo "fun1";
     }
+    function __destruct()
+    {
+        echo "stop";
+    }
+   
 }
 
 // fun1(); it Will Give Error As Function cannot be called From Out side It should Be Called By Making Class First 
 
-
+$obj1=new class1(20);
+$value=$obj1->fun1();
+echo $value;
 ?>
