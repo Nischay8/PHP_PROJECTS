4f8c88c500a62e8dfddfedf31096b2d103b719e7
nischay <nischey.me008@gmail.com>
2023-06-24 17:52:16 +0530

    Added on 24 June 2023

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+class class1{
+    function fun1(){
+        echo "fun1";
+    }
+    function fun2(){
+        echo "fun2";
+    }
+}
+
+// fun1(); it Will Give Error As Function cannot be called From Out side It should Be Called By Making Class First 
+
+
+?>
